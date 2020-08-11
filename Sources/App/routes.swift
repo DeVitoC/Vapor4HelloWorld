d60c3dc6e0d39cfdcc11625177dd42da56d533ac
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-11 08:02:14 -0400

    Added hello, movies/genre/fiction, and movies/genre/comedy routes.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,24 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+
+    // http://127.0.0.1:8080
     app.get { req in
         return "ROOT"
+    }
+
+    // http://127.0.0.1:8080/hello
+    app.get("hello") { req in
+        return "Hello"
+    }
+
+    // http://127.0.0.1:8080/movies/genre/fiction
+    app.get("movies", "genre", "fiction") { req in
+        return "/movies/genre/fiction"
+    }
+
+    // http://127.0.0.1:8080/movies/genre/comedy
+    app.get("movies", "genre", "comedy") { req in
+        return "/movies/genre/fiction"
     }
 }
