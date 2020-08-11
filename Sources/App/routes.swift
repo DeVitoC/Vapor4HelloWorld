d8b70f44bfe0efe73fa6892e194f93a943e0f798
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-11 08:14:52 -0400

    added movies/genre/yourgenrehere, and movies/year/yourhear/genre/yourgenrehere routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,6 +12,25 @@ func routes(_ app: Application) throws {
         return "Hello"
     }
 
+    // http://127.0.0.1:8080/movies/year/youryear/genreyourgenrehere
+    app.get("movies", "year", ":year", "genre", ":genre") { req -> String in
+        guard let genre = req.parameters.get("genre"), let year = req.parameters.get("year") else {
+            throw Abort(.badRequest)
+        }
+
+        return "The genre was \(genre) and the year was \(year)"
+    }
+
+    // http://127.0.0.1:8080/movies/genre/yourgenrehere
+    app.get("movies", "genre", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.badRequest)
+        }
+
+        return "The Genre was \(name)"
+    }
+
+    /*
     // http://127.0.0.1:8080/movies/genre/fiction
     app.get("movies", "genre", "fiction") { req in
         return "/movies/genre/fiction"
@@ -21,4 +40,6 @@ func routes(_ app: Application) throws {
     app.get("movies", "genre", "comedy") { req in
         return "/movies/genre/fiction"
     }
+ */
+
 }
