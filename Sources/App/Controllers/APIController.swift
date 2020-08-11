07c0f44276dcb729d4535ea91bdf2b208a0b8d97
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-11 09:18:58 -0400

    Added User and Address models, and returned uses models to generate a user JSON object.

--- Sources/App/Controllers/APIController.swift
+++ Sources/App/Controllers/APIController.swift
@@ -8,6 +8,18 @@
 import Foundation
 import Vapor
 
+struct User: Content {
+    let name: String
+    let age: Int
+    let address: Address
+}
+
+struct Address: Content {
+    let street: String
+    let state: String
+    let zipcode: String
+}
+
 struct APIController: RouteCollection {
 
     func boot(routes: RoutesBuilder) throws {
@@ -16,10 +28,13 @@ struct APIController: RouteCollection {
         api.get("users", use: getUsers)
     }
 
-    func getUsers(req: Request) throws -> Response {
-        let users = [["name":"Alex","age":32], ["name":"Mary","age":56]]
-        let data = try JSONSerialization.data(withJSONObject: users, options: .prettyPrinted)
-
-        return Response(status: .ok, body: Response.Body(data: data))
+    func getUsers(req: Request) throws -> [User] {
+//        let users = [["name":"Alex","age":32], ["name":"Mary","age":56]]
+//        let data = try JSONSerialization.data(withJSONObject: users, options: .prettyPrinted)
+//
+//        return Response(status: .ok, body: Response.Body(data: data))
+        let address = Address(street: "1200 Richmond Ave", state: "TX", zipcode: "77098")
+        let user = User(name: "John Doe", age: 32, address: address)
+        return [user]
     }
 }
